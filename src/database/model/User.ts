35a5a7ae32ee792d7a5a44d9663b4af67453aa71
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-11-15 17:41:47 -0600

    Add user and plant mongo schemes

--- src/database/model/User.ts
+++ src/database/model/User.ts
@@ -1,1 +1,10 @@
 import { Schema } from "mongoose";
+import { PlantSchema } from "./Plant";
+
+export const userSchema = new Schema({
+  defaultLocation: String,
+  favorites: [PlantSchema],
+  name: String,
+  email: String,
+  password: String,
+});
