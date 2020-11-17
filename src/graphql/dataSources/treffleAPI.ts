eaacff20a496436e7a16f991a1553f0f56b8bb9f
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-11-16 20:15:31 -0600

    Create method to fetch random page from trefle api

--- src/graphql/dataSources/treffleAPI.ts
+++ src/graphql/dataSources/treffleAPI.ts
@@ -1,17 +1,26 @@
 import { RESTDataSource } from "apollo-datasource-rest";
 import { TREFFLE_API_KEY } from "../../env";
 
-export interface PlantInfo {
+export interface TrefleTypes {
   id: number;
   common_name?: string;
-  scientific_name: string;
-  image_url: string;
-  family_common_name: string;
+  scientific_name?: string;
+  image_url?: string;
+  family_common_name?: string;
   family: string;
 }
 
+export interface IPlant {
+  id: number;
+  commonName?: string;
+  scientificName: string;
+  imageUrl: string;
+  familyCommonName: string;
+  familyScientificName: string;
+}
+
 class TreffleAPI extends RESTDataSource {
-  readonly token: string;
+  private readonly token: string;
   readonly nativePlantFilter: string;
 
   constructor() {
@@ -21,25 +30,38 @@ class TreffleAPI extends RESTDataSource {
     this.nativePlantFilter = "filter%5Bestablishment%5D=native";
   }
 
-  buildDistributionUrl(page?: number, state = "COL"): string {
+  private buildDistributionUrl(page?: number, state = "COL"): string {
     const url = `v1/distributions/${state}/plants?${this.nativePlantFilter}&${this.token}`;
     return page ? `${url}&page=${page}` : url;
   }
 
-  async getPlantList(page?: number, state?: string): Promise<PlantInfo[]> {
-    try {
-      const { data } = await this.get(this.buildDistributionUrl(page, state));
-
-      return data.map((plant: PlantInfo) => ({
+  private formatData(plantData: TrefleTypes[]) {
+    return plantData.map((plant: TrefleTypes) => ({
         id: plant.id,
         commonName: plant.common_name,
         scientificName: plant.scientific_name,
         imageUrl: plant.image_url,
         familyCommonName: plant.family_common_name,
         familyScientificName: plant.family,
-      }));
+      }))
+  }
+
+  async getPlantList(page?: number, state?: string): Promise<IPlant[]> {
+    try {
+      const { data } = await this.get(this.buildDistributionUrl(page, state));
+      return this.formatData(data)
     } catch (error) {
       return error.message;
+    }
+  }
+
+  async getRandomPlantPage(): Promise<IPlant[]> {
+    const randomNumber = Math.floor(Math.random() * 100);
+    try {
+      const {data} = await this.get(this.buildDistributionUrl(randomNumber))
+      return this.formatData(data)
+    } catch (err) {
+      return err.message
     }
   }
 }
