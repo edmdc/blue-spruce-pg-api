fe104c6c776a526066e1310d58c56186c751760c
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-11-16 18:35:43 -0600

    Connect user creation to mutation returned result

--- src/graphql/resolvers/user.ts
+++ src/graphql/resolvers/user.ts
@@ -8,14 +8,6 @@ export const userSignUp = async (
   _sources: any,
   { name, email, password }: ArgTypes,
   { models, db }: any
-): Promise<void> => {
-  let user;
-  try {
-    user = await models.User.signUp(name, email, password, db);
-    console.log(user);
-    return user;
-  } catch (err) {
-    console.log(err);
-  }
-  return user;
+): Promise<User> => {
+  return models.User.signUp(name, email, password, db);
 };
