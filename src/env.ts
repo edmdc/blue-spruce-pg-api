fa1ef803c9aebc547682ce7fea28b9874a8d4dd5
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-11-28 14:28:14 -0600

    Add production scripts and PORT env variable

--- src/env.ts
+++ src/env.ts
@@ -2,4 +2,4 @@ import Dotenv from "dotenv";
 
 Dotenv.config();
 
-export const { TREFFLE_API_KEY, MONGO_DB_URL, MONGO_DB_NAME } = process.env;
+export const { TREFFLE_API_KEY, MONGO_DB_URL, PORT } = process.env;
